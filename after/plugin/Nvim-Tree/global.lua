0f63c841a85e086ceb0648368d5907e5b1904089
CatboyJeans <davidg0704@hotmail.com>
2023-08-13 14:58:31 -0600

    refactor: Changed nvim-tree <leader>t to <leader>e

--- after/plugin/Nvim-Tree/global.lua
+++ after/plugin/Nvim-Tree/global.lua
@@ -1,4 +1,4 @@
 -- global
 -- I wont disable netrw yet since tree-less workflow is also pog
-vim.api.nvim_set_keymap("n", "<leader>t", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
 
