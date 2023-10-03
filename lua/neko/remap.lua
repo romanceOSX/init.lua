eebfe0af28bf6024cfc167f8fe25b1c15d3dda55
CatboyJeans <davidg0704@hotmail.com>
2023-10-02 22:21:29 -0600

    feat: move visual line mode

--- lua/neko/remap.lua
+++ lua/neko/remap.lua
@@ -3,3 +3,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
 
 vim.keymap.set('i','<C-c>','<Esc>')
+
+--- vim.keymap.set('n','<C-f>',"<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -105,10 +105,8 @@ _G.packer_plugins = {
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["indent-blankline.nvim"] = {
-    load_after = {},
     loaded = true,
-    needs_bufread = false,
-    path = "/home/torres/.local/share/nvim/site/pack/packer/opt/indent-blankline.nvim",
+    path = "/home/torres/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lsp-zero.nvim"] = {
@@ -199,11 +197,6 @@ time([[Defining packer_plugins]], false)
 time([[Config for toggleterm.nvim]], true)
 try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
 time([[Config for toggleterm.nvim]], false)
--- Load plugins in order defined by `after`
-time([[Sequenced loading]], true)
-vim.cmd [[ packadd oxocarbon.nvim ]]
-vim.cmd [[ packadd indent-blankline.nvim ]]
-time([[Sequenced loading]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
