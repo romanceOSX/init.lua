93291034bbc47e192a10a27f9e7378d6d4394e19
CatboyJeans <davidg0704@hotmail.com>
2023-10-20 15:30:06 -0600

    feat: support for luadoc, dockerfile and cmake lsp

--- after/plugin/autocompletion.lua
+++ after/plugin/autocompletion.lua
@@ -8,7 +8,8 @@ lsp.ensure_installed({
 	'lua_ls',
 	'clangd',
 	'pyright',
-    'neocmake',
+    'cmake',
+    'dockerls',
 })
 
 -------------------------------------------------------------------------------------
