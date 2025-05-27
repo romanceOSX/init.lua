301e777f76f5db6042881bb6f7bb709fe639c217
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-27 12:37:57 -0600

    feat: migrated remaps

--- lua/romance/core/remaps.lua
+++ lua/romance/core/remaps.lua
@@ -1,1 +1,22 @@
-print("Hello!")
+-- core nvim remaps
+
+-- <leader> is defined here
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+
+vim.keymap.set('i','<C-c>','<Esc>')
+-- Clear Highlights
+
+vim.keymap.set("n", "<Esc>", "<cmd> noh <CR>")
+--- vim.keymap.set('n','<C-f>',"<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- move blocks of lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Buffer navigation
+vim.keymap.set("n", "<leader>gb", ":buffers<CR>:buffer <Space>")
+
+-- Marks navigation
+vim.keymap.set("n", "<leader>gm", ":marks<CR> <Space>")                         -- This is wrong
+
