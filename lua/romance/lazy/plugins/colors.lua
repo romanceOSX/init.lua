6ac54238df311c715ec02e8c28d84204d274895d
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-27 22:28:22 -0600

    feat: initial color module

---
+++ lua/romance/lazy/plugins/colors.lua
@@ -0,0 +1,13 @@
+--> color module
+
+return {
+    "rockyzhang24/arctic.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    name = "arctic",
+    branch = "main",
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme koehler")
+    end
+}
+
