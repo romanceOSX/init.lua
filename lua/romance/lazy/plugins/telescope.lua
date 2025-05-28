78f3f60675b30291d48518089ef9924ce9aee6c2
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-27 20:13:55 -0600

    refactor: changed keys inside keys lazy field

--- lua/romance/lazy/plugins/telescope.lua
+++ lua/romance/lazy/plugins/telescope.lua
@@ -1,3 +1,7 @@
+-- Telescope config
+
+local builtin = require('telescope.builtin')
+
 return {
     {
         "nvim-telescope/telescope.nvim",
@@ -5,30 +9,27 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim"
         },
-        config = function()
-            require('telescope').setup({})
-
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>pws', function()
+        keys = {
+            {'<leader>pf', builtin.find_files},
+            {'<C-p>', builtin.git_files},
+            {'<leader>pws', function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word })
-            end)
-            vim.keymap.set('n', '<leader>pWs', function()
+            end},
+            {'<leader>pWs', function()
                 local word = vim.fn.expand("<cWORD>")
                 builtin.grep_string({ search = word })
-            end)
-            vim.keymap.set('n', '<leader>ps', function()
+            end},
+            {'<leader>ps', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end)
-            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        end
+            end},
+            {'n', '<leader>vh', builtin.help_tags, {}},
+        }
     },
 
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' 
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
     }
 }
 
