44d8a50499848afb2f33cd770fc6fd3a4defc1d1
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-29 19:33:13 -0600

    feat: added everforest

--- lua/romance/lazy/plugins/colors.lua
+++ lua/romance/lazy/plugins/colors.lua
@@ -1,13 +1,16 @@
 --> color module
 
 return {
-    "rockyzhang24/arctic.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    name = "arctic",
-    branch = "main",
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme lunaperche")
-    end
+    {
+        "sainnhe/everforest",
+        config = function()
+            vim.g.everforest_background = 'medium'
+            vim.g.everforest_better_performance = 1
+            vim.g.everforest_enable_italic = 1
+            vim.g.everforest_transparent_background = 1
+            vim.g.everforest_dim_inactive_windows = 1
+            vim.cmd("colorscheme everforest")
+        end
+    },
 }
 
