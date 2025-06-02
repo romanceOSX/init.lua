096be00c84f98f4cb00c9e109accab8bb1c4e7b4
romance <106858958+romanceOSX@users.noreply.github.com>
2025-06-01 18:28:34 -0600

    feat: initial support for python venvs

---
+++ lua/romance/lazy/plugins/python.lua
@@ -0,0 +1,22 @@
+-- python-related config module
+
+return {
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        lazy = false,
+        branch = "regexp", -- This is the regexp branch, use this for the new version
+        opts = {
+            search_venv_managers = true,
+            search = true,
+        },
+        keys = {
+            {"<leader>vs", "<cmd>VenvSelect<cr>", { desc = "Select Python venv" }}
+        }
+    }
+}
+
