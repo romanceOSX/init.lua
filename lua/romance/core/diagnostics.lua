92d442c24a77f58d0a8c2bf2b9ebb0b102fb7ae2
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-29 07:35:34 -0600

    chore: diagnostics

--- lua/romance/core/diagnostics.lua
+++ lua/romance/core/diagnostics.lua
@@ -1,7 +1,27 @@
+vim.diagnostic.config({
+    --virtual_text = true,
+    -- update_in_insert = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
+})
 
 -- toggle diagnostics, picked up from :h vim.diagnostic
 vim.keymap.set('n', '<leader>dd', function()
     local new_config = not vim.diagnostic.config().virtual_lines
     vim.diagnostic.config({ virtual_lines = new_config })
 end, { desc = 'Toggle diagnostic virtual_lines' })
+
+-- keymaps
+vim.keymap.set("n", "<leader>dj", function()
+    vim.diagnostic.jump{count = 1}
+end)
+vim.keymap.set("n", "<leader>dk", function()
+    vim.diagnostic.jump{count = -1}
+end)
 
