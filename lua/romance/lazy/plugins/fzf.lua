f277f10b370e05824a1fd8fd7656e8bf0357b169
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-31 20:23:43 -0600

    fix: worked for some reason

--- lua/romance/lazy/plugins/fzf.lua
+++ lua/romance/lazy/plugins/fzf.lua
@@ -1,9 +1,13 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "nvim-mini/mini.icons" },
-  opts = {}
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "nvim-mini/mini.icons" },
+    opts = {},
+    config = function(plug, table)
+        local fzf = require("fzf-lua")
+        vim.keymap.set("n", '<leader>pf', fzf.files)
+    end
 }
 
