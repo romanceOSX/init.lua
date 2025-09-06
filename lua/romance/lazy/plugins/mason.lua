54d86f4e959807ce1f0287c59bcee91404dedf96
romance <106858958+romanceOSX@users.noreply.github.com>
2025-09-06 17:03:15 -0600

    feat: mason lazy module

---
+++ lua/romance/lazy/plugins/mason.lua
@@ -0,0 +1,37 @@
+
+return {
+    -- The core mason package manager and UI
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        }
+    },
+    -- provides lsp autoconfiguration from the Mason-provided servers
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            --automatic_enable = false
+            -- NOTE: how does this work? say if I have clangd pre-installed, does that mean that
+            --       lspconfig calls it automatically?? how does priority works with mason?
+            --       will it run mason-enabled first or the system-wide installation
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "pyright",
+                "clangd",
+            },
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    }
+}
+
