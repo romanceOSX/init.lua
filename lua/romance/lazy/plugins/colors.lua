59e0dd681121db888e1d3bab75df329bf5835ee0
romance <106858958+romanceOSX@users.noreply.github.com>
2025-09-17 21:51:59 -0600

    chore: updating colors

--- lua/romance/lazy/plugins/colors.lua
+++ lua/romance/lazy/plugins/colors.lua
@@ -9,13 +9,68 @@
 return {
     {
         "sainnhe/everforest",
-        config = function()
+        enabled = false,
+        config = function ()
             vim.g.everforest_background = 'medium'
             vim.g.everforest_better_performance = 1
             vim.g.everforest_enable_italic = 1
             vim.g.everforest_transparent_background = 1
             vim.g.everforest_dim_inactive_windows = 1
-            vim.cmd("colorscheme everforest")
+            --vim.cmd("colorscheme everforest")
+        end
+    },
+    {
+        'matsuuu/pinkmare',
+        lazy = false,
+        enabled = false,
+        priority = 1000,
+        init = function ()
+            vim.opt.termguicolors = true
+            vim.g.pinkmare_transparent_background = 1
+        end,
+        config = function ()
+            vim.g.pinkmare_palette = {
+                bg_green = {"#1e25eb", "22", "DarkGreen"},
+            }
+            vim.cmd.colorscheme("pinkmare")
+        end,
+    },
+    {
+        "rijulpaul/nightblossom.nvim",
+        name = "nightblossom",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            variant = "pastel",
+            transparent = true,
+            integrations = {
+                treesitter = true,
+            },
+            --- Overrides
+            -- highlights = {
+            --     Normal = { bg = "#1a1a1a", fg = "#e0e0e0" },
+            --     ["@comment"] = { fg = "#888888", italic = false },
+            -- }
+        },
+        config = function(plugin, opts)
+            require("nightblossom").setup(opts)
+            vim.cmd("colorscheme nightblossom-pastel")
+        end,
+    },
+    {
+        "anAcc22/sakura.nvim",
+        dependencies = "rktjmp/lush.nvim",
+        enabled = true,
+        lazy = false,
+        config = function()
+            vim.opt.background = "dark" -- or "light"
+            vim.cmd('colorscheme sakura') -- sets the colorscheme
+            vim.cmd [[
+              highlight Normal guibg=none
+              highlight NonText guibg=none
+              highlight Normal ctermbg=none
+              highlight NonText ctermbg=none
+            ]]
         end
     },
 }
