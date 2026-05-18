8cf7451f5fec62ce74452bf04288698dd97d174d
romance <106858958+romanceOSX@users.noreply.github.com>
2026-05-17 19:39:18 -0600

    chore: markup lua module

---
+++ lua/romance/lazy/plugins/markup.lua
@@ -0,0 +1,32 @@
+--> https://github.com/MeanderingProgrammer/render-markdown.nvim
+--> https://github.com/iamcco/markdown-preview.nvim
+
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        ft = { "markdown" },
+        opts = {
+            enabled = true,
+        },
+        config = function(_, opts)
+            require("render-markdown").setup(opts)
+
+            vim.keymap.set("n", "<leader>mr", "<cmd>RenderMarkdown toggle<cr>", { desc = "Toggle render-markdown" })
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        config = function()
+            vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
+        end,
+    },
+}
