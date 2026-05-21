539d1909e52e305bb0c79f3be8a77aa94512122d
romance <106858958+romanceOSX@users.noreply.github.com>
2026-05-20 21:33:22 -0600

    chore: formatting changes

--- lua/romance/lazy/plugins/terminal.lua
+++ lua/romance/lazy/plugins/terminal.lua
@@ -1,9 +1,49 @@
--- Toggle-term
--- TODO: check for more viable options
+--> https://github.com/akinsho/toggleterm.nvim
 
 return {
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  opts = { }
-}
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+        { "<C-\\>", desc = "Toggle floating terminal" },
+        { "<leader>th", desc = "Toggle horizontal terminal" },
+        { "<leader>tg", desc = "Toggle lazygit" },
+    },
+    opts = {
+        open_mapping = [[<C-\>]],
+        direction = "float",
+        -- always open at project root
+        dir = "git_dir",
+        float_opts = {
+            border = "curved",
+            winblend = 5,
+        },
+        -- keep terminal hidden rather than closed so state persists
+        hide_numbers = true,
+        shade_terminals = false,
+        start_in_insert = true,
+        persist_mode = true,
+    },
+    config = function(_, opts)
+        local toggleterm = require("toggleterm")
+        toggleterm.setup(opts)
 
+        local Terminal = require("toggleterm.terminal").Terminal
+
+        -- horizontal split terminal (good for watching build output)
+        local hsplit = Terminal:new({ direction = "horizontal", dir = "git_dir" })
+        vim.keymap.set("n", "<leader>th", function() hsplit:toggle() end, { desc = "Toggle horizontal terminal" })
+
+        -- lazygit (only registers the map if lazygit is installed)
+        if vim.fn.executable("lazygit") == 1 then
+            local lazygit = Terminal:new({
+                cmd = "lazygit",
+                direction = "float",
+                dir = "git_dir",
+                float_opts = { border = "curved" },
+                -- close the window automatically when lazygit exits
+                on_exit = function(t) t:close() end,
+            })
+            vim.keymap.set("n", "<leader>tg", function() lazygit:toggle() end, { desc = "Toggle lazygit" })
+        end
+    end,
+}
